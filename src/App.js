3df2cf84f3c76167d520fcc743b0952588200518
nickidahl <6417280@rts-365.dk>
2020-09-17 14:12:54 +0200

    Theme dropdown farveskifte

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-
+  const themeHook = useState("darkblue");
   return (
     <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
         <div id="something-important">
           <header>
             <Link to="/">Adopt me!</Link>
           </header>
-            <Router>
-              <SearchParams path="/"  />
-              <Details path="/details/:id" />
-            </Router>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
         </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
